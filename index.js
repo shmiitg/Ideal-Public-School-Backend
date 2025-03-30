dd355ffb8474173d7271cabf56d84a1109b41ad4
Shubham Mondal <shubhamank002@gmail.com>
2025-03-30 22:05:24 +0530

    update index file

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.get("/", (req, res) => {
 
 app.use("/student", studentRouter);
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running at port ${port}`));
+// app.listen(port, () => console.log(`Server running at port ${port}`));
+
+module.exports = app;
